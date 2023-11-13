d2834b14ee9e37f115ea007855fb04a38267120b
tuando18 <tuaansdo.18@gmail.com>
2023-11-13 21:38:50 +0700

    Tuan up gd frg dt 13/11/2023

---
+++ app/src/main/java/anhpvph37030/fpoly/duan_nhom8/fragment/DienThoaiFrg.java
@@ -0,0 +1,26 @@
+package anhpvph37030.fpoly.duan_nhom8.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import anhpvph37030.fpoly.duan_nhom8.R;
+
+
+public class DienThoaiFrg extends Fragment {
+
+    public DienThoaiFrg() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dien_thoai_frg, container, false);
+    }
+}
