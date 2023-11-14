2a78ebbf4805eb6f0e7c9cefed6b25c99672810e
Vietanh <anhpvph37030@fpt.edu.vn>
2023-11-14 12:08:01 +0700

    giao dien ca nhan

---
+++ app/src/main/java/anhpvph37030/fpoly/duan_nhom8/fragment/CaNhan.java
@@ -0,0 +1,28 @@
+package anhpvph37030.fpoly.duan_nhom8.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import anhpvph37030.fpoly.duan_nhom8.R;
+
+
+public class CaNhan extends Fragment {
+
+
+
+    public CaNhan() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ca_nhan, container, false);
+    }
+}
