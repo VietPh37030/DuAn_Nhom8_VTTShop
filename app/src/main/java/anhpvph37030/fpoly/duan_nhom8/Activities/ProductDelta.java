8488fb1075452eb84167e2b26830f882def6740f
Vietanh <anhpvph37030@fpt.edu.vn>
2023-11-17 23:10:51 +0700

    111

--- app/src/main/java/anhpvph37030/fpoly/duan_nhom8/Activities/ProductDelta.java
+++ app/src/main/java/anhpvph37030/fpoly/duan_nhom8/Activities/ProductDelta.java
@@ -1,8 +1,10 @@
-package anhpvph37030.fpoly.duan_nhom8;
+package anhpvph37030.fpoly.duan_nhom8.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import anhpvph37030.fpoly.duan_nhom8.R;
 
 public class ProductDelta extends AppCompatActivity {
 
