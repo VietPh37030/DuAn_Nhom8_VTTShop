b7ac176d67f502c60707172170fb8f08869883c1
tuando18 <tuaansdo.18@gmail.com>
2023-11-17 23:09:51 +0700

    Tuan up gd dt 17/11/2023

--- app/src/main/java/anhpvph37030/fpoly/duan_nhom8/fragment/MainFrg.java
+++ app/src/main/java/anhpvph37030/fpoly/duan_nhom8/fragment/MainFrg.java
@@ -1,5 +1,6 @@
 package anhpvph37030.fpoly.duan_nhom8.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import anhpvph37030.fpoly.duan_nhom8.Adapter.ProductAdapter;
+import anhpvph37030.fpoly.duan_nhom8.ProductDelta;
 import anhpvph37030.fpoly.duan_nhom8.R;
 import anhpvph37030.fpoly.duan_nhom8.model.Product;
 
@@ -27,12 +30,12 @@ public class MainFrg extends Fragment {
 
         // Tạo danh sách sản phẩm
         List<Product> productList = new ArrayList<>();
-        productList.add(new Product(R.drawable.item_phone,"Product 1", "10.000.000 đ"));
-        productList.add(new Product(R.drawable.item_phone,"Product 2", "10.000.000 đ"));
-        productList.add(new Product(R.drawable.item_phone,"Product 3", "10.000.000 đ"));
-        productList.add(new Product(R.drawable.item_phone,"Product 1", "10.000.000 đ"));
-        productList.add(new Product(R.drawable.item_phone,"Product 2", "10.000.000 đ"));
-        productList.add(new Product(R.drawable.item_phone,"Product 3", "10.000.000 đ"));
+        productList.add(new Product(R.drawable.item_phone, "Product 1", "10.000.000 đ"));
+        productList.add(new Product(R.drawable.item_phone, "Product 2", "10.000.000 đ"));
+        productList.add(new Product(R.drawable.item_phone, "Product 3", "10.000.000 đ"));
+        productList.add(new Product(R.drawable.item_phone, "Product 1", "10.000.000 đ"));
+        productList.add(new Product(R.drawable.item_phone, "Product 2", "10.000.000 đ"));
+        productList.add(new Product(R.drawable.item_phone, "Product 3", "10.000.000 đ"));
         // Thêm thông tin cho các sản phẩm khác
         // Tạo Adapter
         ProductAdapter adapter = new ProductAdapter(getActivity(), productList);
@@ -40,7 +43,22 @@ public class MainFrg extends Fragment {
         // Liên kết Adapter với GridView
         GridView gridView = v.findViewById(R.id.gvDT);
         gridView.setAdapter(adapter);
+        // Xử lý sự kiện khi người dùng chọn một sản phẩm
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Lấy sản phẩm được chọn từ danh sách
+                Product selectedProduct = productList.get(position);
 
+                // Tạo Intent để chuyển từ MainFrg sang ProductDetailActivity
+                Intent intent = new Intent(getActivity(), ProductDelta.class);
+
+                // Đặt thông tin sản phẩm vào Intent
+
+                // Chuyển sang ProductDetailActivity
+                startActivity(intent);
+            }
+        });
         return v;
 
     }
