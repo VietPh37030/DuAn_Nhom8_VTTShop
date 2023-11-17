d23277601d146c75c30e85c68ef10bb84a7142a9
tuando18 <tuaansdo.18@gmail.com>
2023-11-17 20:55:16 +0700

    Tuan up gd dt 17/11/2023

--- app/src/main/java/anhpvph37030/fpoly/duan_nhom8/fragment/MainFrg.java
+++ app/src/main/java/anhpvph37030/fpoly/duan_nhom8/fragment/MainFrg.java
@@ -7,8 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import anhpvph37030.fpoly.duan_nhom8.Adapter.ProductAdapter;
 import anhpvph37030.fpoly.duan_nhom8.R;
+import anhpvph37030.fpoly.duan_nhom8.model.Product;
 
 
 public class MainFrg extends Fragment {
@@ -17,6 +23,26 @@ public class MainFrg extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_frg, container, false);
+        View v = inflater.inflate(R.layout.fragment_main_frg, container, false);
+
+        // Tạo danh sách sản phẩm
+        List<Product> productList = new ArrayList<>();
+        productList.add(new Product(R.drawable.item_phone,"Product 1", "10.000.000 đ"));
+        productList.add(new Product(R.drawable.item_phone,"Product 2", "10.000.000 đ"));
+        productList.add(new Product(R.drawable.item_phone,"Product 3", "10.000.000 đ"));
+        productList.add(new Product(R.drawable.item_phone,"Product 1", "10.000.000 đ"));
+        productList.add(new Product(R.drawable.item_phone,"Product 2", "10.000.000 đ"));
+        productList.add(new Product(R.drawable.item_phone,"Product 3", "10.000.000 đ"));
+        // Thêm thông tin cho các sản phẩm khác
+
+        // Tạo Adapter
+        ProductAdapter adapter = new ProductAdapter(getActivity(), productList);
+
+        // Liên kết Adapter với GridView
+        GridView gridView = v.findViewById(R.id.gvDT);
+        gridView.setAdapter(adapter);
+
+        return v;
+
     }
 }
