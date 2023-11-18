d170e06e53eaca8f364fbb26d4c578854ddfd0d2
Vietanh <anhpvph37030@fpt.edu.vn>
2023-11-18 21:57:33 +0700

    layout 1 số màn hình âdmin

---
+++ app/src/main/java/anhpvph37030/fpoly/duan_nhom8/fragment/AdminQL.java
@@ -0,0 +1,35 @@
+package anhpvph37030.fpoly.duan_nhom8.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import anhpvph37030.fpoly.duan_nhom8.R;
+
+
+public class AdminQL extends Fragment {
+
+
+
+    public AdminQL() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_admin, container, false);
+    }
+}
