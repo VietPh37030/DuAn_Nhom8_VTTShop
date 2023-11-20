0bcda019639698a28a207a6f250c6941919aff9d
DOTIENTHANH <ngan20030807@gmail.com>
2023-11-20 21:27:25 +0700

    nut exit tren toolbar doi mk voi doi thong tin

--- app/src/main/java/anhpvph37030/fpoly/duan_nhom8/Activities/HoaDonActivity.java
+++ app/src/main/java/anhpvph37030/fpoly/duan_nhom8/Activities/HoaDonActivity.java
@@ -2,17 +2,26 @@ package anhpvph37030.fpoly.duan_nhom8.Activities;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import anhpvph37030.fpoly.duan_nhom8.R;
 
 
 public class HoaDonActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoadon);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.dangxuat);
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
 }
