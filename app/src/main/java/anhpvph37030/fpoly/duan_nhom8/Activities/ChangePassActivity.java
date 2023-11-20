12de3288ba5c48d0df51379d6641173b0fe4fed9
Vietanh <anhpvph37030@fpt.edu.vn>
2023-11-20 12:06:51 +0700

    đổi mật khẩu phải xác nhận email

--- app/src/main/java/anhpvph37030/fpoly/duan_nhom8/Activities/ChangePassActivity.java
+++ app/src/main/java/anhpvph37030/fpoly/duan_nhom8/Activities/ChangePassActivity.java
@@ -1,95 +1,108 @@
-package anhpvph37030.fpoly.duan_nhom8.Activities;
+package anhpvph37030.fpoly.duan_nhom8.Activities;// ChangePasswordActivity.java
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 import anhpvph37030.fpoly.duan_nhom8.R;
 
 public class ChangePassActivity extends AppCompatActivity {
-    TextInputLayout txtmk, txtmkm, txtrepass;
-    Button btnchangepass;
-    private ProgressDialog progressDialog;
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private FirebaseUser currentUser;
+
+    private TextInputLayout edtOldPassword, edtNewPassword, edtConfirmPassword;
+    private Button btnConfirm, btnChangePassword;
+    TextView txttag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_pass);
-        init();
-        btnchangepass.setOnClickListener(new View.OnClickListener() {
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        edtOldPassword = findViewById(R.id.edt_changepass_mkc);
+        edtNewPassword = findViewById(R.id.edt_changepass_mkm);
+        edtConfirmPassword = findViewById(R.id.edt_changepass_nhaplaimk);
+        btnConfirm = findViewById(R.id.btn_sendotp);
+        btnChangePassword = findViewById(R.id.btnchangepass_thaydoi);
+        TextView txttag = findViewById(R.id.txttag);
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onclickChange();
+                sendConfirmationEmail();
+            }
+        });
+
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changePassword();
             }
         });
     }
 
-
-    private void init() {
-        progressDialog = new ProgressDialog(ChangePassActivity.this);
-        txtmk = findViewById(R.id.edt_changepass_mkc);
-        txtmkm = findViewById(R.id.edt_changepass_mkm);
-        txtrepass = findViewById(R.id.edt_changepass_nhaplaimk);
-        btnchangepass = findViewById(R.id.btnchangepass_thaydoi);
-    }
-
-    private void onclickChange() {
-        String pass = txtmk.getEditText().getText().toString();
-        String mkm = txtmkm.getEditText().getText().toString();
-        String repass = txtrepass.getEditText().getText().toString();
-        progressDialog.show();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), pass);
-        user.reauthenticate(credential)
+    private void sendConfirmationEmail() {
+        String email = currentUser.getEmail();
+        // Gửi email xác nhận đến email người dùng
+        currentUser.sendEmailVerification()
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
-                            // Mật khẩu cũ đúng, kiểm tra mật khẩu mới và nhập lại
-                            if (mkm.equals(repass)) {
-                                // Thực hiện thay đổi mật khẩu
-                                progressDialog.setMessage("Đang thay đổi mật khẩu...");
-                                progressDialog.show();
-
-                                user.updatePassword(mkm)
-                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<Void> task) {
-                                                progressDialog.dismiss();
-                                                if (task.isSuccessful()) {
-                                                    Toast.makeText(ChangePassActivity.this, "Thay Đổi Mật Khẩu Thành Công", Toast.LENGTH_SHORT).show();
-                                                    finish(); // Kết thúc Activity sau khi thay đổi mật khẩu thành công
-                                                } else {
-                                                    Toast.makeText(ChangePassActivity.this, "Thay Đổi Mật Khẩu Thất Bại", Toast.LENGTH_SHORT).show();
-                                                }
-                                            }
-                                        });
-                            } else {
-                                // Mật khẩu mới và nhập lại không khớp
-                                Toast.makeText(ChangePassActivity.this, "Mật khẩu mới và nhập lại không khớp", Toast.LENGTH_SHORT).show();
-                            }
+                            Toast.makeText(ChangePassActivity.this, "Vui lòng xác nhận email trước khi đổi mật khẩu", Toast.LENGTH_SHORT).show();
+                            // Ẩn trường nhập mật khẩu cũ và nút xác nhận
+                            edtOldPassword.setVisibility(View.GONE);
+                            btnConfirm.setVisibility(View.GONE);
+                            txttag.setVisibility(View.GONE);
+                            // Hiển thị trường nhập mật khẩu mới và nút thay đổi
+                            edtNewPassword.setVisibility(View.VISIBLE);
+                            edtConfirmPassword.setVisibility(View.VISIBLE);
+                            btnChangePassword.setVisibility(View.VISIBLE);
                         } else {
-                            // Mật khẩu cũ không đúng
-                            Toast.makeText(ChangePassActivity.this, "Mật khẩu cũ không đúng", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(ChangePassActivity.this, "Lỗi khi gửi email xác nhận", Toast.LENGTH_SHORT).show();
+                            btnConfirm.setVisibility(View.VISIBLE);
                         }
                     }
                 });
+    }
+
+    private void changePassword() {
+        String newPassword = edtNewPassword.getEditText().getText().toString();
+        String confirmPassword = edtConfirmPassword.getEditText().getText().toString();
+
+        if (newPassword.equals(confirmPassword)) {
+            currentUser.updatePassword(newPassword)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(ChangePassActivity.this, "Đổi mật khẩu thành công", Toast.LENGTH_SHORT).show();
+                                finish();
+                            } else {
+                                Toast.makeText(ChangePassActivity.this, "Lỗi khi đổi mật khẩu", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        } else {
+            Toast.makeText(ChangePassActivity.this, "Mật khẩu mới không khớp", Toast.LENGTH_SHORT).show();
+        }
     }
 }
