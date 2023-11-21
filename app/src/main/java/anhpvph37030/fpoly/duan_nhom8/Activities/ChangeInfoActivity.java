5fbf58251d011d631d073554b5280a402c624ba0
Vietanh <anhpvph37030@fpt.edu.vn>
2023-11-21 22:07:55 +0700

    thay đổi thông tin cá nhân

--- app/src/main/java/anhpvph37030/fpoly/duan_nhom8/Activities/ChangeInfoActivity.java
+++ app/src/main/java/anhpvph37030/fpoly/duan_nhom8/Activities/ChangeInfoActivity.java
@@ -1,30 +1,58 @@
 package anhpvph37030.fpoly.duan_nhom8.Activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.appcompat.app.ActionBar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import anhpvph37030.fpoly.duan_nhom8.MainActivity;
 import anhpvph37030.fpoly.duan_nhom8.R;
 
 public class ChangeInfoActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private DatabaseReference userRef;
+    private String currentUserId;
+
     Toolbar toolbar;
+    ImageView imgavt;
+    EditText edthovaten, edtemail;
+    Button btnsubmit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_info);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        currentUserId = currentUser.getUid();
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
+
         Toolbar toolbar = findViewById(R.id.toolbardoitt);
         setSupportActionBar(toolbar);
+        initUI();
 
-        // Đặt Navigation Icon là icon Exit
         toolbar.setNavigationIcon(R.drawable.back);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,12 +60,105 @@ public class ChangeInfoActivity extends AppCompatActivity {
                 exitApp();
             }
         });
+
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUserInfo();
+            }
+        });
+
+        imgavt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
     }
+
+    private void initUI() {
+        imgavt = findViewById(R.id.imgavtinfo);
+        edthovaten = findViewById(R.id.edt_change_hvt);
+        edtemail = findViewById(R.id.edt_change_diachi);
+        btnsubmit = findViewById(R.id.btnchange_thaydoi);
+
+        // Hiển thị thông tin cá nhân hiện tại
+        edthovaten.setText(currentUser.getDisplayName());
+        edtemail.setText(currentUser.getEmail());
+        // Load ảnh đại diện hiện tại (nếu có)
+        // Glide.with(this).load(currentUser.getPhotoUrl()).placeholder(R.drawable.default_avatar).into(imgavt);
+    }
+
     private void exitApp() {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
         finish();
+    }
 
+    private void updateUserInfo() {
+        String newDisplayName = edthovaten.getText().toString().trim();
+        String newEmail = edtemail.getText().toString().trim();
+
+        if (!newDisplayName.isEmpty()) {
+            userRef.child("displayName").setValue(newDisplayName);
+            currentUser.updateProfile(new UserProfileChangeRequest.Builder()
+                    .setDisplayName(newDisplayName)
+                    .build());
+        }
+
+        if (!newEmail.isEmpty()) {
+            currentUser.updateEmail(newEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        userRef.child("email").setValue(newEmail);
+                    }
+                }
+            });
+        }
+
+        // Hiển thị thông báo cập nhật thành công hoặc xử lý lỗi nếu cần
+        Toast.makeText(this, "Thông tin đã được cập nhật", Toast.LENGTH_SHORT).show();
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            updateProfileImage(imageUri);
+        }
+    }
+
+    private void updateProfileImage(Uri imageUri) {
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("profile_images").child(currentUserId + ".jpg");
+
+        storageRef.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()) {
+                    storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String imageUrl = uri.toString();
+                            userRef.child("profileImage").setValue(imageUrl);
+                            currentUser.updateProfile(new UserProfileChangeRequest.Builder()
+                                    .setPhotoUri(uri)
+                                    .build());
+                            // Hiển thị ảnh đại diện mới (nếu muốn)
+                            // Glide.with(ChangeInfoActivity.this).load(uri).placeholder(R.drawable.default_avatar).into(imgavt);
+                        }
+                    });
+                }
+            }
+        });
     }
 }
--- app/src/main/java/anhpvph37030/fpoly/duan_nhom8/fragment/CaNhanFrg.java
+++ app/src/main/java/anhpvph37030/fpoly/duan_nhom8/fragment/CaNhanFrg.java
@@ -4,34 +4,48 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.appcompat.app.AlertDialog;
-import androidx.cardview.widget.CardView;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import anhpvph37030.fpoly.duan_nhom8.Activities.ChangeInfoActivity;
 import anhpvph37030.fpoly.duan_nhom8.Activities.ChangePassActivity;
 import anhpvph37030.fpoly.duan_nhom8.R;
 import anhpvph37030.fpoly.duan_nhom8.taikhoan.Login;
-
 
 public class CaNhanFrg extends Fragment {
     ImageView imgavt;
     TextView txtht, txtemailcn;
     TextView btndangxuat;
     TextView cthaydoimk;
-    public CaNhanFrg() {
+    TextView cthaydoithongtin;
 
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private DatabaseReference userRef;
+
+    public CaNhanFrg() {
+        // Required empty public constructor
     }
 
     @Override
@@ -39,12 +53,20 @@ public class CaNhanFrg extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_ca_nhan, container, false);
-        imgavt = (ImageView) v.findViewById(R.id.imageView2);
-        txtht = (TextView) v.findViewById(R.id.txtttHoten);
+
+        imgavt = v.findViewById(R.id.imageView2);
+        txtht = v.findViewById(R.id.txtttHoten);
         txtemailcn = v.findViewById(R.id.txtttemail);
         cthaydoimk = v.findViewById(R.id.nav_repasswork);
+        cthaydoithongtin = v.findViewById(R.id.nav_Bill_ltru);
         btndangxuat = v.findViewById(R.id.btndangxuat);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUser.getUid());
+
         showUserInfo();
+
         btndangxuat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,6 +77,7 @@ public class CaNhanFrg extends Fragment {
                 builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        mAuth.signOut();
                         Toast.makeText(getActivity(), "Đăng xuất thành công", Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(getActivity(), Login.class);
                         startActivity(intent);
@@ -63,55 +86,99 @@ public class CaNhanFrg extends Fragment {
                 builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        // Do nothing
                     }
                 });
                 builder.show();
             }
         });
+
         cthaydoimk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =  new Intent(getActivity(), ChangePassActivity.class);
+                Intent intent = new Intent(getActivity(), ChangePassActivity.class);
                 startActivity(intent);
-
             }
         });
+
+        cthaydoithongtin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ChangeInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imgavt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
         return v;
-
     }
-
 
     private void showUserInfo() {
-        // Lấy thông tin người dùng hiện tại từ FirebaseAuth
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String name = snapshot.child("displayName").getValue(String.class);
+                    String email = snapshot.child("email").getValue(String.class);
+                    String profileImage = snapshot.child("profileImage").getValue(String.class);
 
-        // Nếu không có người dùng hiện tại, hàm sẽ kết thúc
-        if (user == null) {
-            return;
-        }
+                    txtht.setText(name);
+                    txtemailcn.setText(email);
 
-        // Lấy thông tin người dùng từ đối tượng FirebaseUser, bao gồm tên, email và URL ảnh đại diện
-        String name = user.getDisplayName();
-        String email = user.getEmail();
-        Uri photoUrl = user.getPhotoUrl();
+                    // Sử dụng Glide để tải và hiển thị ảnh đại diện
+                    if (profileImage != null && !profileImage.isEmpty()) {
+                        Glide.with(CaNhanFrg.this).load(profileImage).placeholder(R.drawable.avt).into(imgavt);
+                    }
+                }
+            }
 
-        // Nếu tên người dùng là null, ẩn TextView hiển thị tên
-        if (name == null) {
-            txtht.setVisibility(View.GONE);
-        } else {
-            // Nếu tên người dùng không phải là null, hiển thị TextView và đặt tên người dùng
-            txtht.setVisibility(View.VISIBLE);
-            txtht.setText(name);
-        }
-
-        // Đặt email người dùng cho TextView txtemailcn
-        txtemailcn.setText(email);
-
-        // Sử dụng thư viện Glide để tải ảnh đại diện từ URL và hiển thị nó trong ImageView imgavt
-        // Nếu không tải được ảnh, sẽ hiển thị ảnh mặc định (R.drawable.avt)
-
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Xử lý khi có lỗi xảy ra trong truy vấn dữ liệu
+            }
+        });
     }
 
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, 1);
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == getActivity().RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            updateProfileImage(imageUri);
+        }
+    }
+
+    private void updateProfileImage(Uri imageUri) {
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("profile_images").child(currentUser.getUid() + ".jpg");
+
+        storageRef.putFile(imageUri).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                storageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                    String imageUrl = uri.toString();
+                    userRef.child("profileImage").setValue(imageUrl);
+                    currentUser.updateProfile(new UserProfileChangeRequest.Builder()
+                            .setPhotoUri(uri)
+                            .build());
+
+                    Glide.with(this).load(uri).placeholder(R.drawable.avt).into(imgavt);
+                    Toast.makeText(getActivity(), "Ảnh đại diện đã được cập nhật", Toast.LENGTH_SHORT).show();
+                });
+            } else {
+                Toast.makeText(getActivity(), "Có lỗi xảy ra khi cập nhật ảnh", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
