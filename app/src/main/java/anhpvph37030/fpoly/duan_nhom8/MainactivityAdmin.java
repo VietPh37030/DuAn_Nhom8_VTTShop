8776843c2b5eefb79294a896e76889799e178871
DOTIENTHANH <ngan20030807@gmail.com>
2023-11-22 22:13:36 +0700

    hien thi thanh tim kiem vs nut button len trang chu admin

--- app/src/main/java/anhpvph37030/fpoly/duan_nhom8/MainactivityAdmin.java
+++ app/src/main/java/anhpvph37030/fpoly/duan_nhom8/MainactivityAdmin.java
@@ -1,14 +1,79 @@
 package anhpvph37030.fpoly.duan_nhom8;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.ArrayList;
+
+import anhpvph37030.fpoly.duan_nhom8.Adapter.MenuAdapter;
+import anhpvph37030.fpoly.duan_nhom8.fragment.AdminQL;
+import anhpvph37030.fpoly.duan_nhom8.fragment.CaNhanFrg;
+import anhpvph37030.fpoly.duan_nhom8.fragment.MainFrg;
+import anhpvph37030.fpoly.duan_nhom8.fragment.MyCartFrg;
 
 public class MainactivityAdmin extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView_adm;
+    MenuAdapter adapter;
+    ViewPager2 pagerMain_adm;
+    ArrayList<Fragment> list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainadmin);
+        initView_adm();
+    }
+
+    private void initView_adm() {
+        pagerMain_adm = findViewById(R.id.pagerMain_adm);
+        bottomNavigationView_adm = findViewById(R.id.menu_nav_adm);
+        list.add(new AdminQL());
+        adapter = new MenuAdapter(this, list);
+        pagerMain_adm.setAdapter(adapter);
+        pagerMain_adm.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        bottomNavigationView_adm.setSelectedItemId(R.id.nav_trangtru);
+                        break;
+                    case 1:
+                        bottomNavigationView_adm.setSelectedItemId(R.id.nav_sanpham);
+                        break;
+                    case 2:
+                        bottomNavigationView_adm.setSelectedItemId(R.id.nav_thongke);
+                        break;
+                    case 3:
+                        bottomNavigationView_adm.setSelectedItemId(R.id.nav_tai_khoan);
+                        break;
+                }
+                super.onPageSelected(position);
+            }
+        });
+        bottomNavigationView_adm.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nav_trangtru) {
+                    pagerMain_adm.setCurrentItem(0);
+                }
+                if (item.getItemId() == R.id.nav_sanpham) {
+                    pagerMain_adm.setCurrentItem(1);
+                }
+                if (item.getItemId() == R.id.nav_thongke) {
+                    pagerMain_adm.setCurrentItem(2);
+                }
+                if (item.getItemId() == R.id.nav_tai_khoan) {
+                    pagerMain_adm.setCurrentItem(3);
+                }
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/anhpvph37030/fpoly/duan_nhom8/fragment/AdminQL.java
+++ app/src/main/java/anhpvph37030/fpoly/duan_nhom8/fragment/AdminQL.java
@@ -2,29 +2,51 @@ package anhpvph37030.fpoly.duan_nhom8.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import anhpvph37030.fpoly.duan_nhom8.R;
 
 
 public class AdminQL extends Fragment {
 
-    public AdminQL() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
+    RecyclerView recyclerView;
+    ImageButton btnThem;
+    SearchView searchView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main_admin, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_admin, container, false);
+        searchView = view.findViewById(R.id.search_view);
+        btnThem = view.findViewById(R.id.btnThem);
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Xử lý sự kiện ở đây
+            }
+        });
+
+        // Xử lý sự kiện khi thanh tìm kiếm thay đổi
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Xử lý khi người dùng nhấn Enter trên bàn phím
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // Xử lý khi người dùng thay đổi nội dung trong thanh tìm kiếm
+                return false;
+            }
+        });
+        return view;
     }
 }
